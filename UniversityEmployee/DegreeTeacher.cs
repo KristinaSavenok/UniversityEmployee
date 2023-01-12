b2a5faa7830998919791ca7f8cac1f853b41f05d
KristinaSavenok <k.ilyenka@gmail.com>
2023-01-13 00:23:07 +0100

    Task2

---
+++ UniversityEmployee/DegreeTeacher.cs
@@ -0,0 +1,20 @@
+﻿namespace UniversityEmployee
+{
+    internal class DegreeTeacher : Teacher
+    {
+        public string ScienceDegree { get; set; }
+        public string TitleTeaching { get; set; }
+
+        public DegreeTeacher(Person person, int taxID, Course course, string scienceDegree, string titleTeaching) : base(person, taxID, course)
+        {
+            ScienceDegree = scienceDegree;
+            TitleTeaching = titleTeaching;
+        }
+
+        public override string GetOfficialDuties()
+        {
+            return $"Teacher {PersonEmployee.FirstName} {PersonEmployee.LastName} has {ScienceDegree} {TitleTeaching} degree";
+        }
+    }
+}
+
--- UniversityEmployee/Program.cs
+++ UniversityEmployee/Program.cs
@@ -1,10 +1,45 @@
 ﻿using UniversityEmployee;
-Person person1 = new Person("Ivan","Popov");
-person1.GetInfo();
 
-Person person2 = new Person();
-person2.GetInfo();
+Teacher mathTeacher = new Teacher(
+    new Person("Ivan", "Popov"), 5, new Course("Math", "2+2=4"));
+Teacher englishTeacher = new Teacher(
+    new Person("Katsiaryna", "Umnaja"), 99, new Course("English", "london is the capital of great britain"));
 
-UniversityEmployeeItem employee1 = new UniversityEmployeeItem(person1, 10);
-employee1.TaxID = 10;
+DegreeTeacher chemistryTeacher = new DegreeTeacher(
+    new Person("Vasil", "Opytnyj"), 77, new Course("Chemistry", "H2O"), "Master", "Professor");
+DegreeTeacher lawTeacher = new DegreeTeacher(
+    new Person("Yury", "Big"), 3, new Course("Jurisprudence", "court of law"), "Dr", "Assistant Professor");
 
+SupportStaff security = new SupportStaff(
+    new Person("Ivan", "Glavnyj"), 100, "Security", "Security");
+SupportStaff cleaner = new SupportStaff(
+    new Person("Valentina", "Czistaja"), 11, "Cleaner", "Cleaning");
+SupportStaff driver = new SupportStaff(
+    new Person("Dominic", "Toretto"), 999, "Driver", "Fast Road");
+
+UniversityEmployeeItem[] employeesArray = new UniversityEmployeeItem[]
+{
+    mathTeacher,
+    englishTeacher,
+    chemistryTeacher,
+    lawTeacher,
+    security,
+    cleaner,
+    driver
+};
+
+Console.WriteLine("list of all employees:");
+for (int i = 0; i < employeesArray.Length; i++)
+{
+    Console.WriteLine(employeesArray[i].GetOfficialDuties());
+}
+
+Console.WriteLine("list of teachers:");
+for (int i = 0; i < employeesArray.Length; i++)
+{
+    if (employeesArray[i] is DegreeTeacher)
+    {
+        Console.WriteLine(employeesArray[i].GetOfficialDuties());
+    }
+}
+    
